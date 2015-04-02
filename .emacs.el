a51658627963d32b090100f82ba5323d45216535
Alex Dunn <dunn.alex@gmail.com>
2015-04-01 17:33:32 -0700

    emacs: install packages when necessary
    also general cleanup

--- .emacs.el
+++ .emacs.el
@@ -14,16 +14,18 @@
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 
+;; Do the packaging dance
 (require 'package)
 (package-initialize)
-
-;; MELPA
 (add-to-list 'package-archives
   '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(package-refresh-contents)
 
-;; appearance
-(add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/emacs-color-theme-solarized")
-(load-theme 'solarized t)
+;; Solarized theme
+(if (not (package-installed-p 'color-theme-sanityinc-solarized))
+    (progn
+      (package-install 'color-theme-sanityinc-solarized)))
+(load-theme 'sanityinc-solarized-dark t)
 
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
@@ -33,31 +35,37 @@
 (global-linum-mode 1)
 (column-number-mode 1)
 
-;; commenting
+;; Commenting
 (global-set-key "\C-c;"      'comment-region)
 (global-set-key "\C-c:"      'uncomment-region)
 
-;; kill whitespace
+;; Kill whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; new lines
+;; Add new lines automatically
 (setq next-line-add-newlines t)
 
-;; display redraw
+;; Faster redrawing
 (setq redisplay-dont-pause nil)
 
-;; tabs (no)
+;; Tabs (no)
 (setq-default indent-tabs-mode nil)
 
-;; auto fill mode
+;; Auto fill mode
 ;; (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'tex-mode-hook 'turn-on-auto-fill)
 (add-hook 'markdown-mode-hook 'turn-on-auto-fill)
 
-;; flycheck
+;; Flycheck
+(if (not (package-installed-p 'flycheck))
+    (progn
+      (package-install 'flycheck)))
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;;;; better Meta
+;; Better Meta
+(if (not (package-installed-p 'smex))
+    (progn
+      (package-install 'smex)))
 (setq mac-option-modifier 'meta)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x C-m") 'smex)
@@ -67,12 +75,15 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (setq smex-show-unbound-commands t)
 
+;; Multiple cursors
 ;; https://github.com/kjhealy/emacs-starter-kit/blob/master/starter-kit-bindings.org
-;; When you have an active region that spans multiple lines, the following will add a cursor to each line:
+;; When you have an active region that spans multiple lines, the
+;; following will add a cursor to each line:
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
 (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
-;; When you want to add multiple cursors not based on continuous lines, but based on keywords in the buffer, use:
+;; When you want to add multiple cursors not based on continuous
+;; lines, but based on keywords in the buffer, use:
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
@@ -85,107 +96,12 @@
 (global-set-key (kbd "H-S-n") 'mc/mark-more-like-this-extended)
 (global-set-key (kbd "H-S-a") 'mc/mark-all-in-region)
 
-;; pdf tools
-(require 'pdf-view)
-(require 'pdf-util)
-(require 'pdf-info)
-(require 'cus-edit)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-(add-hook 'html-mode-hook 'ac-html-enable)
-(add-to-list 'web-mode-ac-sources-alist
-  '("html" . (
-               ;; attribute-value better to be first
-               ac-source-html-attribute-value
-               ac-source-html-tag
-               ac-source-html-attribute)))
-
-
-(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
-(add-hook 'mail-mode-hook 'turn-on-auto-fill)
-
-;; tex, latex, markdown modes
-(add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
- (autoload 'markdown-mode "markdown-mode.el"
-       "Major mode for editing Markdown files" t)
-     (setq auto-mode-alist
-     (cons '("\\.text" . markdown-mode) auto-mode-alist))
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-
-;;(require 'fountain-mode)
-(add-to-list 'auto-mode-alist '("\\.fountain$" . fountain-mode))
-
-;; sass and haml
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
-(add-hook 'scss-mode-hook 'scss-rainbow-hook)
-(defun scss-rainbow-hook ()
-  (rainbow-mode 1))
-
-;; php-mode
-(add-to-list 'load-path "~/.emacs.d/vendor/php-mode")
-(require 'php-mode)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-
-;; textmate minor mode
-(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-(require 'textmate)
-(textmate-mode)
-
-;; js2 mode
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("iojs" . js2-mode))
-
-(require 'go-mode-autoloads)
-
-(add-to-list 'load-path "~/.emacs.d/vendor/full-ack")
-(autoload 'ack-same "full-ack" nil t)
-(autoload 'ack "full-ack" nil t)
-(autoload 'ack-find-same-file "full-ack" nil t)
-(autoload 'ack-find-file "full-ack" nil t)
-
-;; spelling
+;; Spelling
 (setq-default ispell-program-name "aspell")
 (add-to-list 'load-path "/usr/local/bin")
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
 (autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
 (autoload 'tex-mode-flyspell-verify "flyspell" "" t)
-
-;; table mode
-(require 'table)
-(add-hook 'text-mode-hook 'table-recognize)
-(add-hook 'markdown-mode-hook 'table-recognize)
-
-;; Set to the location of your Org files on your local system
-(setq org-directory "~/org")
-;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
-;; Set to <your Dropbox root directory>/MobileOrg.
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-
-;; org-mode
-;; The following lines are always needed.  Choose your own keys.
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(add-hook 'org-mode-hook 'turn-on-flyspell 'append)
 
 ;; case handling
 (put 'downcase-region 'disabled nil)
@@ -193,12 +109,13 @@
 
 ;; word count
 (autoload 'word-count-mode "word-count"
-          "Minor mode to count words." t nil)
+  "Minor mode to count words."
+  t nil)
 (global-set-key "\M-+" 'word-count-mode)
 
 ;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
 (defun unfill-paragraph ()
-  "Takes a multi-line paragraph and makes it into a single line of text."
+  "Take a multi-line paragraph and make it into a single line of text."
   (interactive)
   (let ((fill-column (point-max)))
     (fill-paragraph nil)))
@@ -221,6 +138,118 @@
   (interactive)
   (insert "¯\\_(ツ)_/¯"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; table mode
+(require 'table)
+(add-hook 'text-mode-hook 'table-recognize)
+(add-hook 'markdown-mode-hook 'table-recognize)
+
+;; Mail mode
+(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+(add-hook 'mail-mode-hook 'turn-on-auto-fill)
+
+;; Web mode
+(if (not (package-installed-p 'web-mode))
+    (progn
+      (package-install 'web-mode)))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; HTML autocomplete
+(if (not (package-installed-p 'ac-html))
+    (progn
+      (package-install 'ac-html)))
+(add-hook 'html-mode-hook 'ac-html-enable)
+(add-to-list 'web-mode-ac-sources-alist
+  '("html" . (
+               ;; attribute-value better to be first
+               ac-source-html-attribute-value
+               ac-source-html-tag
+               ac-source-html-attribute)))
+
+;; Markdown mode
+(if (not (package-installed-p 'markdown-mode))
+    (progn
+      (package-install 'markdown-mode)))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; Fountain mode
+(if (not (package-installed-p 'fountain-mode))
+    (progn
+      (package-install 'fountain-mode)))
+(add-to-list 'auto-mode-alist '("\\.fountain$" . fountain-mode))
+
+;; SCSS mode
+(if (not (package-installed-p 'scss-mode))
+    (progn
+      (package-install 'scss-mode)))
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
+
+;; Rainbow mode
+(if (not (package-installed-p 'rainbow-mode))
+    (progn
+      (package-install 'rainbow-mode)))
+(add-hook 'scss-mode-hook 'scss-rainbow-hook)
+(defun scss-rainbow-hook ()
+  "Colorize color strings."
+  (rainbow-mode 1))
+
+;; PHP mode
+(if (not (package-installed-p 'php-mode))
+    (progn
+      (package-install 'php-mode)))
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+;; JS mode
+(if (not (package-installed-p 'js2-mode))
+    (progn
+      (package-install 'js2-mode)))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("iojs" . js2-mode))
+
+;; Go mode
+(if (not (package-installed-p 'go-mode))
+    (progn
+      (package-install 'go-mode)))
+(require 'go-mode-autoloads)
+
+;; textmate minor mode
+;; (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+;; (require 'textmate)
+;; (textmate-mode)
+
+;; ;; Set to the location of your Org files on your local system
+;; (setq org-directory "~/org")
+;; ;; Set to the name of the file where new notes will be stored
+;; (setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; ;; Set to <your Dropbox root directory>/MobileOrg.
+;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+;; org-mode
+(if (not (package-installed-p 'org))
+    (progn
+      (package-install 'org)))
+;; The following lines are always needed.  Choose your own keys.
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-flyspell 'append)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -242,7 +271,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#fdf6e3" :foreground "#657b83" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "monotype" :family "Consolas"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "monotype" :family "Consolas"))))
  '(js2-error ((t (:foreground "#dc322f"))))
  '(js2-external-variable ((t (:foreground "#cb4b16"))))
  '(js2-function-call ((t (:inherit default :foreground "#6c71c4"))))
