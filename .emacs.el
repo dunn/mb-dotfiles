38ab619a41c01bc99802aad57e1452e3930db15a
Alex Dunn <dunn.alex@gmail.com>
2015-05-13 18:50:21 -0700

    emacs: simplify PATH

--- .emacs.el
+++ .emacs.el
@@ -6,8 +6,8 @@
 ;;; Code:
 ;; import PATH etc; necessary since emacs starts as a daemon before
 ;; .bash_profile is run
-(setenv "PATH" "/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:~/.cabal/bin")
-(setq exec-path (append exec-path '("/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:~/.cabal/bin")))
+(setenv "PATH" "/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")
+(setq exec-path (append exec-path '("/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")))
 
 ;; https://github.com/magnars/.emacs.d/blob/master/init.el
 (setq inhibit-startup-message t)
