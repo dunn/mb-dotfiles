3c5e8960a0f4d0c81f52e1afd523ab24fd0632b1
Alex Dunn <dunn.alex@gmail.com>
2015-05-22 21:46:31 -0700

    emacs: move `customize` rules into separate file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
+ '(frame-background-mode nil)
+ '(js2-basic-offset 2)
+ '(magit-use-overlays nil)
+ '(markdown-enable-math t)
+ '(menu-bar-mode t)
+ '(perl-indent-level 2)
+ '(require-final-newline t)
+ '(scss-compile-at-save nil)
+ '(x-stretch-cursor t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "monotype" :family "Consolas"))))
+ '(js2-error ((t (:foreground "#dc322f"))))
+ '(js2-external-variable ((t (:foreground "#cb4b16"))))
+ '(js2-function-call ((t (:inherit default :foreground "#6c71c4"))))
+ '(js2-function-param ((t (:foreground "#d33682" :slant italic))))
+ '(js2-instance-member ((t (:foreground "#d33682"))))
+ '(js2-jsdoc-html-tag-delimiter ((t (:foreground "#859900"))))
+ '(js2-jsdoc-html-tag-name ((t (:foreground "#b58900")))))
