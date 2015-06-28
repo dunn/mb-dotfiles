76884011aae09107f29f49ef6273e08f9f1ae199
Alex Dunn <dunn.alex@gmail.com>
2015-06-28 16:44:08 -0700

    emacs: key shortcut for magit-dispatch-popup

--- .emacs.el
+++ .emacs.el
@@ -75,6 +75,7 @@
 ;; brew install magit
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 ;; editorconfig
