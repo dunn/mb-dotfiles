d5fd9222656e50fd50d78c88e085f6401d4cecad
Alex Dunn <dunn.alex@gmail.com>
2015-07-20 12:31:13 -0700

    move emacs stuff into a directory and add Brewfile
    to install emacs packages required by .emacs.el:
    
    ```
    brew tap homebrew/bundle
    brew bundle --file=emacs/Brewfile
    ```

--- emacs/.emacs
+++ emacs/.emacs
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 alias ebug='$(which emacs) --debug-init'
-alias gomacs='$(which emacs) --daemon'
+alias gomacs='$(which emacs) --debug-init --daemon'
 alias emac="emacsclient -t -a emacs"
 alias emacs="emacsclient -c -a emacs"
-alias emcas=emacs
-alias emca=emac
+alias emcas=emacs # i can't type
 alias semac="sudo emacsclient -t -a emacs"
 alias semacs="sudo emacsclient -c -a emacs"
 alias boop='emacs . &'
+
+fleemacs(){
+  kill -3 "$(ps aux | ag emac.*daemon | awk '{ print $2 }' | sort -n | head -1)"
+}
--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,11 +1,13 @@
 ;;; package --- emacs configuration
 ;;; Commentary:
+;; Have emacs --daemon run at startup; otherwise the package
+;; initialization will slow every launch
 
 ;;; Code:
 ;; import PATH etc; necessary since emacs starts as a daemon before
 ;; .bash_profile is run
-(setenv "PATH" "/home/honker/bin:/home/honker/.linuxbrew/opt/go/libexec/bin:/home/honker/.linuxbrew/sbin:/home/honker/.linuxbrew/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")
-(setq exec-path (append exec-path '("/home/honker/bin:/home/honker/.linuxbrew/opt/go/libexec/bin:/home/honker/.linuxbrew/sbin:/home/honker/.linuxbrew/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")))
+(setenv "PATH" "/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")
+(setq exec-path (append exec-path '("/Users/cat/bin:/usr/local/opt/go/libexec/bin:/usr/local/sbin:/usr/local/bin:~/.cabal/bin::/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin")))
 
 ;; https://github.com/magnars/.emacs.d/blob/master/init.el
 (setq inhibit-startup-message t)
@@ -17,15 +19,29 @@
 (load custom-file)
 
 ;; Homebrew executables and lisp files
-(add-to-list 'load-path "/home/honker/.linuxbrew/bin/")
-(let ((default-directory "/home/honker/.linuxbrew/share/emacs/site-lisp/"))
+(add-to-list 'load-path "/usr/local/bin/")
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 
+;; Solarized theme
+;; https://github.com/sellout/emacs-color-theme-solarized/issues/141#issuecomment-71862293
+(add-to-list 'custom-theme-load-path "/usr/local/share/emacs/site-lisp/solarized")
+;; `t` is important: http://stackoverflow.com/a/8547861
+(load-theme 'solarized t)
+(setq solarized-termcolors 256)
+(add-to-list 'default-frame-alist '(background-mode . dark))
+
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
 (blink-cursor-mode 0)
 
 ;; http://ergoemacs.org/emacs/emacs_make_modern.html
 (global-linum-mode 1)
 (column-number-mode 1)
+
+;; Commenting
+(global-set-key "\C-c;"      'comment-region)
+(global-set-key "\C-c:"      'uncomment-region)
 
 ;; Kill whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -39,70 +55,6 @@
 ;; Tabs (no)
 (setq-default indent-tabs-mode nil)
 
-;; case handling
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-;; allow 'y' or 'n' instead of 'yes' or 'no'
-;; http://www.cs.berkeley.edu/~prmohan/emacs/
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; fix emoji support in cocoa-mode
-;; https://github.com/dunn/company-emoji/issues/2#issue-99494790
-(defun darwin-set-emoji-font (frame)
-"Adjust the font settings of FRAME so Emacs NS/Cocoa can display emoji properly."
-  (if (eq system-type 'darwin)
-    (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)))
-;; For when emacs is started with Emacs.app
-(darwin-set-emoji-font nil)
-;; Hook for when a cocoa frame is created with emacsclient
-;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
-(add-hook 'after-make-frame-functions 'darwin-set-emoji-font)
-
-;;;;;;;;;;;;;;;;
-;; KEYBINDINGS
-;;;;;;;;;;;;;;;;
-;; some inspiration from https://masteringemacs.org/article/my-emacs-keybindings
-
-(setq mac-option-modifier 'meta)
-
-;; mimic my tmux bindings, sort of
-(define-key key-translation-map "\C-j" "\C-x")
-(global-set-key "\M-o" 'other-window)
-(global-set-key "\C-xj" 'other-window)
-(global-set-key "\C-x;" 'other-window)
-
-(global-set-key "\C-x\C-a" 'mark-whole-buffer)
-(global-unset-key "\C-xh")
-
-(global-set-key "\C-xk" 'kill-this-buffer)
-(global-set-key "\C-x\C-k" 'kill-buffer)
-
-(define-key global-map (kbd "C-+") 'text-scale-increase)
-(define-key global-map (kbd "C--") 'text-scale-decrease)
-
-(global-set-key "\C-c;" 'comment-region)
-(global-set-key "\C-c:" 'uncomment-region)
-
-(global-set-key "\C-cz" 'shell)
-(global-set-key "\C-x\C-b" 'ibuffer)
-(global-set-key "\C-co" 'browse-url-at-point)
-
-;; see below
-(global-set-key "\C-cs" 'shruggie)
-
-;; mimic native Mac OS behavior
-(global-set-key "\M-_" 'mdash)
-
-;; I also accidentally set column instead of opening a file
-;; https://www.gnu.org/software/emacs/manual/html_node/eintr/Keybindings.html#Keybindings
-(global-unset-key "\C-xf")
-(define-key key-translation-map "\C-xf" "\C-x\C-f")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (require 'ido)
 (ido-mode t)
 
@@ -111,157 +63,43 @@
 (add-hook 'tex-mode-hook 'turn-on-auto-fill)
 (add-hook 'markdown-mode-hook 'turn-on-auto-fill)
 
-;; word count
-(autoload 'word-count-mode "word-count"
-  "Minor mode to count words."
-  t nil)
-(global-set-key "\M-+" 'word-count-mode)
-
-;; table mode
-(require 'table)
-(add-hook 'text-mode-hook 'table-recognize)
-(add-hook 'markdown-mode-hook 'table-recognize)
-
-;; Mail mode
-(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
-(add-hook 'mail-mode-hook 'turn-on-auto-fill)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; HOMEBREW FORMULAE
-;; brew tap homebrew/bundle
-;; brew tap dunn/emacs
-;; brew bundle --file=emacs/Brewfile
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; https://github.com/sellout/emacs-color-theme-solarized/issues/141#issuecomment-71862293
-(add-to-list 'custom-theme-load-path "/home/honker/.linuxbrew/share/emacs/site-lisp/solarized")
-;; `t` is important: http://stackoverflow.com/a/8547861
-(load-theme 'solarized t)
-(setq solarized-termcolors 256)
-(setq frame-background-mode 'light)
-
-(setq-default ispell-program-name "aspell")
-(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
-(autoload 'tex-mode-flyspell-verify "flyspell" "" t)
-
-;; editorconfig needs its hand held with a special exec-path and load-path
-(setq exec-path (append exec-path '("/home/honker/.linuxbrew/opt/editorconfig/bin")))
-(add-to-list 'load-path "/home/honker/.linuxbrew/opt/editorconfig-emacs/share/emacs/site-lisp/editorconfig")
-(load "editorconfig")
-
-;; installed --with-toc
-(require 'markdown-mode)
-(require 'markdown-toc)
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-
+;; magit
+;; brew install magit
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(add-hook 'org-mode-hook 'turn-on-flyspell 'append)
+;; editorconfig
+;; needs its hand held with a special load-path
+(add-to-list 'load-path "/usr/local/opt/editorconfig-emacs/share/emacs/site-lisp/editorconfig-emacs")
+(load "editorconfig")
 
-(require 'gitattributes-mode)
-(require 'gitconfig-mode)
-(require 'gitignore-mode)
-(add-to-list 'auto-mode-alist '("^\\.gitattributes$" . gitattributes-mode))
-(add-to-list 'auto-mode-alist '("^\\.gitconfig$" . gitconfig-mode))
-(add-to-list 'auto-mode-alist '("^\\.gitignore$" . gitignore-mode))
-(add-to-list 'auto-mode-alist '("\\.git\/info\/attributes$" . gitignore-mode))
-(add-to-list 'auto-mode-alist '("\\.git\/config$" . gitignore-mode))
-(add-to-list 'auto-mode-alist '("\\.git\/info\/exclude$" . gitignore-mode))
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-(require 'company-emoji)
-(add-hook 'markdown-mode-hook 'company-mode)
-(add-hook 'mail-mode-hook 'company-mode)
-(add-hook 'text-mode-hook 'company-mode)
-(add-to-list 'auto-mode-alist '("^COMMIT_EDITMSG$" . 'company-mode))
-(add-hook 'company-mode-hook 'company-emoji-init)
-
-(require 'fountain-mode)
-(add-to-list 'auto-mode-alist '("\\.fountain$" . fountain-mode))
-
-(require 'scss-mode)
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
-
-(require 'rainbow-mode)
-(add-hook 'scss-mode-hook 'scss-rainbow-hook)
-(defun scss-rainbow-hook ()
-  "Colorize color strings."
-  (rainbow-mode 1))
-
-(require 'php-mode)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("iojs" . js2-mode))
-
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(require 'flycheck-package)
-(eval-after-load 'flycheck
-  '(flycheck-package-setup))
-
-(require 'browse-kill-ring)
-
-(require 'achievements)
-
-(require 'unkillable-scratch)
-(unkillable-scratch 1)
-(setq unkillable-scratch-behavior 'bury)
-
-(require 'applescript-mode)
-(add-to-list 'auto-mode-alist '("\.applescript$" . applescript-mode))
-(add-to-list 'interpreter-mode-alist '("osascript" . applescript-mode))
-
-(require 'debbugs-org)
-(autoload 'debbugs-org "debbugs-org" "" 'interactive)
-(autoload 'debbugs-org-search "debbugs-org" "" 'interactive)
-(autoload 'debbugs-org-bugs "debbugs-org" "" 'interactive)
-
-(require 'register-list)
-(global-set-key "\C-cr" 'register-list)
-
+;; Better Meta
 (require 'smex)
+(setq mac-option-modifier 'meta)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-x C-M") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(setq smex-show-unbound-commands t)
 
-(require 'typo)
-(add-hook 'markdown-mode-hook 'typo-mode)
-(add-hook 'mail-mode-hook 'typo-mode)
-;; typo-mode turns backticks into single left quotes in Markdown, so
-;; we need another way to quickly make code fences:
-(global-set-key "\C-c`" 'code-fence)
+;; Spelling
+(setq-default ispell-program-name "aspell")
+(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
+(autoload 'flyspell-delay-command "flyspell" "Delay on command." t)
+(autoload 'tex-mode-flyspell-verify "flyspell" "" t)
 
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\.yaml$" . yaml-mode))
+;; case handling
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
 
-;;;;;;;;;;;;;;;
-;; FUNCTIONS
-;;;;;;;;;;;;;;;
+;; word count
+(autoload 'word-count-mode "word-count"
+  "Minor mode to count words."
+  t nil)
+(global-set-key "\M-+" 'word-count-mode)
 
 ;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
 (defun unfill-paragraph ()
@@ -276,37 +114,110 @@
   "Insert a lorem ipsum."
   (interactive)
   (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-    "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-    "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-    "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-    "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-    "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-    "culpa qui officia deserunt mollit anim id est laborum."))
+          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
+          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
+          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+          "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+          "culpa qui officia deserunt mollit anim id est laborum."))
 
 (defun shruggie ()
   "Insert him."
   (interactive)
   (insert "¯\\_(ツ)_/¯"))
 
-(defun mdash ()
-  "Insert a dang mdash ok."
-  (interactive)
-  (insert "—"))
-
-(defun code-fence ()
-  "Insert backticks to create a Markdown code block, \
-then set point to the end of the first set of backticks \
-so the code type can be specified."
-  (interactive)
-  (push-mark)
-  (insert "```\n```")
-  (goto-char (- (point) 4)))
-
 (defun autotools ()
   "For Homebrew HEAD builds."
   (interactive)
-  (insert "depends_on \"automake\" => :build\n"
-    "    depends_on \"autoconf\" => :build\n"
-    "    depends_on \"libtool\" => :build"))
+  (insert "    depends_on \"automake\" => :build\n"
+          "    depends_on \"autoconf\" => :build\n"
+          "    depends_on \"libtool\" => :build"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; org-mode (installed from dunn/emacs homebrew tap)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-flyspell 'append)
+
+;; table mode
+(require 'table)
+(add-hook 'text-mode-hook 'table-recognize)
+(add-hook 'markdown-mode-hook 'table-recognize)
+
+;; Mail mode
+(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+(add-hook 'mail-mode-hook 'turn-on-auto-fill)
+
+;; Web mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+;; HTML autocomplete
+(require 'ac-html)
+(add-hook 'html-mode-hook 'ac-html-enable)
+(add-to-list 'web-mode-ac-sources-alist
+  '("html" . (
+               ;; attribute-value better to be first
+               ac-source-html-attribute-value
+               ac-source-html-tag
+               ac-source-html-attribute)))
+
+;; Markdown mode
+;; installed from dunn/emacs --with-markdown-plus
+(require 'markdown-mode)
+(require 'markdown-mode+)
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; Fountain mode
+(require 'fountain-mode)
+(add-to-list 'auto-mode-alist '("\\.fountain$" . fountain-mode))
+
+;; SCSS mode
+(require 'scss-mode)
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
+
+;; Rainbow mode
+(require 'rainbow-mode)
+(add-hook 'scss-mode-hook 'scss-rainbow-hook)
+(defun scss-rainbow-hook ()
+  "Colorize color strings."
+  (rainbow-mode 1))
+
+;; PHP mode
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+;; JS mode
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("iojs" . js2-mode))
+
+;; Flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; ;; Set to the location of your Org files on your local system
+;; (setq org-directory "~/org")
+;; ;; Set to the name of the file where new notes will be stored
+;; (setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; ;; Set to <your Dropbox root directory>/MobileOrg.
+;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+(provide 'emacs)
 ;;; .emacs.el ends here
