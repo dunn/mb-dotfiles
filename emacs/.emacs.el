86fe1b2af5bad6fa59fca0c72a703c0e5aff0086
Alex Dunn <dunn.alex@gmail.com>
2015-07-20 22:38:28 -0700

    emacs.el: remove mobileorg comments

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -220,11 +220,4 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-;; ;; Set to the location of your Org files on your local system
-;; (setq org-directory "~/org")
-;; ;; Set to the name of the file where new notes will be stored
-;; (setq org-mobile-inbox-for-pull "~/org/flagged.org")
-;; ;; Set to <your Dropbox root directory>/MobileOrg.
-;; (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-
 ;;; .emacs.el ends here
