e08b1db8a54692da1af4a6d845b44e66d1f5a819
Alex Dunn <dunn.alex@gmail.com>
2015-07-29 13:11:42 -0700

    yaml-mode.el

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -240,6 +240,10 @@
 (add-hook 'markdown-mode-hook 'typo-mode)
 (add-hook 'mail-mode-hook 'typo-mode)
 
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\.yaml$" . yaml-mode))
+
 ;;;;;;;;;;;;;;;
 ;; FUNCTIONS
 ;;;;;;;;;;;;;;;
