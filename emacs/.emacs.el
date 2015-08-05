84c7370fe32bf167f6b8f942c289ed1f39f2cb03
Alex Dunn <dunn.alex@gmail.com>
2015-08-04 21:15:09 -0700

    emacs: no helm, it messes with my echo area

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -225,12 +225,6 @@
 (add-to-list 'auto-mode-alist '("\.applescript$" . applescript-mode))
 (add-to-list 'interpreter-mode-alist '("osascript" . applescript-mode))
 
-(require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-(require 'emoji-cheat-sheet-plus)
-(global-set-key (kbd "C-c e") 'emoji-cheat-sheet-plus-insert)
-
 (require 'debbugs-org)
 (autoload 'debbugs-org "debbugs-org" "" 'interactive)
 (autoload 'debbugs-org-search "debbugs-org" "" 'interactive)
@@ -239,9 +233,10 @@
 (require 'register-list)
 (global-set-key "\C-cr" 'register-list)
 
-(require 'swiper-helm)
-(global-set-key "\C-s" 'swiper-helm)
-(global-set-key "\C-r" 'swiper-helm)
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-x C-M") 'smex-major-mode-commands)
 
 (require 'typo)
 (add-hook 'markdown-mode-hook 'typo-mode)
