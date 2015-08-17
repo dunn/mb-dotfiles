1bb30aa72a9074c7ba9a30787d670d76e00a33c6
Alex Dunn <dunn.alex@gmail.com>
2015-08-17 12:44:14 -0700

    Revert "gitconfig: no pager for `git d`"
    This reverts commit c7b9ba5f245c720c66f4fa04ee983b2aca4ca098.

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -281,10 +281,6 @@ See https://github.com/jorgenschaefer/circe/wiki/Configuration"
 (add-to-list 'auto-mode-alist '("^COMMIT_EDITMSG$" . 'company-mode))
 (add-hook 'company-mode-hook 'company-emoji-init)
 
-(add-to-list 'load-path "/Users/cat/Dropbox/projects/lisp/homebrew-mode")
-(require 'homebrew-mode)
-(homebrew-mode-default-hooks)
-
 ;;;;;;;;;;;;;;;
 ;; FUNCTIONS
 ;;;;;;;;;;;;;;;
