a9d82ce9a6246fce22be7ddaa1b7795a7f762435
Alex Dunn <dunn.alex@gmail.com>
2015-09-11 01:09:31 -0400

    emacs: homebrew-mode build on linux now

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -293,10 +293,9 @@ See https://github.com/jorgenschaefer/circe/wiki/Configuration"
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
 (if (eq system-type 'darwin)
-  (progn
-    (add-to-list 'load-path "/Users/cat/Dropbox/projects/lisp/homebrew-mode")
-    (require 'homebrew-mode)
-    (global-homebrew-mode)))
+  (add-to-list 'load-path "/Users/cat/Dropbox/projects/lisp/homebrew-mode"))
+(require 'homebrew-mode)
+(global-homebrew-mode)
 
 ;;;;;;;;;;;;;;;
 ;; FUNCTIONS
