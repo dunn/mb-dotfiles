a9bd54ca750a756da3ab76ea8b8a5a0e25df4604
Alex Dunn <dunn.alex@gmail.com>
2015-09-14 11:12:38 -0700

    emacs.el: solarized syntax error

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -382,7 +382,7 @@ clipboard.  This function is only meant to be assigned to \
   (progn
     (setq solarized-termcolors 256)
     (setq frame-background-mode 'light)
-    (load-theme 'solarized t))
+    (load-theme 'solarized t)))
 
 (defun fuck-you ()
   "Because this just doesn't work in Debian Terminal."
