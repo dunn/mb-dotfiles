0bd6ae725e5e90c746a037a1de8ee982b64c6f8d
Alex Dunn <dunn.alex@gmail.com>
2015-09-16 16:45:53 -0700

    emacs.el: reactivate php-mode

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -188,7 +188,7 @@
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
@@ -208,8 +208,8 @@
   "Colorize color strings."
   (rainbow-mode 1))
 
-;; (require 'php-mode)
-;; (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
