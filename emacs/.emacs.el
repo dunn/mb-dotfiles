bde9158b51fb783ef3cf983d0e8fa89572e3d91c
Alex Dunn <dunn.alex@gmail.com>
2015-10-26 08:16:47 -0700

    emacs.el: use web-mode for everything

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -187,7 +187,9 @@
 (add-to-list 'auto-mode-alist '("\\.git\/info\/exclude$" . gitignore-mode))
 
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . web-mode))
+
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
