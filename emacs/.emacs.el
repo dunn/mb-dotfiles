e2ceec277e38b2c1249f7b0b5d0981b158c289c8
Alex Dunn <dunn.alex@gmail.com>
2015-11-02 18:41:18 -0800

    emacs.el: fix solarized path

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -372,7 +372,7 @@ clipboard.  This function is only meant to be assigned to \
   (insert (shell-command-to-string "pbpaste")))
 
 ;; https://github.com/sellout/emacs-color-theme-solarized/issues/141#issuecomment-71862293
-(add-to-list 'custom-theme-load-path (concat --homebrew-prefix "share/emacs/site-lisp/solarized"))
+(add-to-list 'custom-theme-load-path (concat --homebrew-prefix "share/emacs/site-lisp/solarized-emacs"))
 
 ;; no, I don't know why
 (if (eq system-type 'darwin)
