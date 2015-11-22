e8140e4744030962e23c212722f50e2fb15c6caf
Alex Dunn <dunn.alex@gmail.com>
2015-11-22 09:26:44 -0800

    emacs: elisp-slime-nav

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -247,6 +247,10 @@
 (eval-after-load 'company
   '(push 'company-robe company-backends))
 
+(require 'elisp-slime-nav)
+(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (add-hook hook 'elisp-slime-nav-mode))
+
 (require 'diff-hl)
 (global-diff-hl-mode)
 
