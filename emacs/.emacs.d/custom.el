53eef4ad3eb90e19a142901940ea29986817403f
Alex Dunn <dunn.alex@gmail.com>
2015-12-23 08:36:22 -0800

    erc: more default channels

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,30 +3,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(ag-arguments
-     (quote
-       ("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--hidden" "--")))
+ '(ag-arguments
+   (quote
+    ("--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--hidden" "--")))
  '(ag-highlight-search t)
  '(ag-ignore-list (quote (".git")))
  '(as-continuation-offset 2)
  '(as-indent-offset 2)
  '(async-bytecomp-package-mode t)
  '(beacon-size 30)
-  '(company-emoji-aliases
-     (quote
-       ((:man-woman-boy: . ":family:")
-         (:woman-kiss-man: . ":couplekiss:")
-         (:woman_man_holding_hands: . ":couple:")
-         (:woman-heart-man: . ":couple_with_heart:")
-         (:oop: . ":speak_no_evil:")
-         (:middle-finger: . ":reversed-hand-with-middle-finger-extended:"))))
+ '(company-emoji-aliases
+   (quote
+    ((:man-woman-boy: . ":family:")
+     (:woman-kiss-man: . ":couplekiss:")
+     (:woman_man_holding_hands: . ":couple:")
+     (:woman-heart-man: . ":couple_with_heart:")
+     (:oop: . ":speak_no_evil:")
+     (:middle-finger: . ":reversed-hand-with-middle-finger-extended:"))))
  '(company-emoji-insert-unicode t)
  '(company-idle-delay 0.3)
  '(company-minimum-prefix-length 2)
  '(counsel-find-file-at-point t)
  '(css-indent-offset 2)
  '(current-language-environment "UTF-8")
- '(erc-autojoin-channels-alist (quote (("freenode.net" "#machomebrew"))))
+ '(erc-autojoin-channels-alist
+   (quote
+    (("freenode.net" "#machomebrew" "#ansible" "#projecthydra"))))
  '(erc-autojoin-mode t)
  '(erc-button-mode t)
  '(erc-fill-mode t)
@@ -34,9 +36,9 @@
  '(erc-list-mode t)
  '(erc-match-mode t)
  '(erc-menu-mode t)
-  '(erc-modules
-     (quote
-       (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring services stamp track)))
+ '(erc-modules
+   (quote
+    (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring services stamp track)))
  '(erc-move-to-prompt-mode t)
  '(erc-netsplit-mode t)
  '(erc-networks-mode t)
@@ -69,30 +71,30 @@
  '(mark-ring-max 100)
  '(markdown-enable-math t)
  '(menu-bar-mode t)
-  '(message-hidden-headers
-     (quote
-       ("^User-Agent:" "^References:" "^Face:" "^X-Face:" "^X-Draft-From:")))
+ '(message-hidden-headers
+   (quote
+    ("^User-Agent:" "^References:" "^Face:" "^X-Face:" "^X-Draft-From:")))
  '(message-send-mail-function (quote message-send-mail-with-sendmail))
  '(message-sendmail-envelope-from (quote header))
  '(notmuch-address-command "~/bin/lbdbq_no_err")
  '(notmuch-search-oldest-first nil)
-  '(notmuch-tag-formats
-     (quote
-       (("unread"
-          (notmuch-apply-face tag
-            (quote
-              (:foreground "dark cyan"))))
-         ("flagged"
-           (notmuch-tag-format-image-data tag
-             (notmuch-tag-star-icon)))
-         ("pay"
-           (notmuch-apply-face tag
-             (quote
-               (:foreground "light green")))))))
+ '(notmuch-tag-formats
+   (quote
+    (("unread"
+      (notmuch-apply-face tag
+                          (quote
+                           (:foreground "dark cyan"))))
+     ("flagged"
+      (notmuch-tag-format-image-data tag
+                                     (notmuch-tag-star-icon)))
+     ("pay"
+      (notmuch-apply-face tag
+                          (quote
+                           (:foreground "light green")))))))
  '(package-directory-list nil)
-  '(package-selected-packages
-     (quote
-       (cmake-mode notmuch pdf-tools yaml-mode elisp-slime-nav web-mode unkillable-scratch typo scss-mode robe rainbow-mode nlinum markdown-toc magit js2-mode homebrew-mode gitignore-mode gitconfig-mode gitattributes-mode gist fountain-mode flycheck-package flycheck-cask elfeed editorconfig diff-hl counsel company-emoji beacon ag)))
+ '(package-selected-packages
+   (quote
+    (cmake-mode notmuch pdf-tools yaml-mode elisp-slime-nav web-mode unkillable-scratch typo scss-mode robe rainbow-mode nlinum markdown-toc magit js2-mode homebrew-mode gitignore-mode gitconfig-mode gitattributes-mode gist fountain-mode flycheck-package flycheck-cask elfeed editorconfig diff-hl counsel company-emoji beacon ag)))
  '(package-user-dir "")
  '(pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo")
  '(perl-indent-level 2)
@@ -106,9 +108,9 @@
  '(standard-indent 2)
  '(tab-width 2)
  '(tls-checktrust t)
-  '(tls-program
-     (quote
-       ("gnutls-cli --x509cafile /etc/ssl/certs/system.pem -p %p %h")))
+ '(tls-program
+   (quote
+    ("gnutls-cli --x509cafile /etc/ssl/certs/system.pem -p %p %h")))
  '(tramp-connection-timeout 10)
  '(tramp-verbose 6)
  '(typo-language "English")
