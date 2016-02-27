d59b7f189fd86bedfdea0c4a9e3b75b21f11e776
Alex Dunn <dunn.alex@gmail.com>
2016-02-26 20:27:05 -0800

    emacs: set up slime

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -367,6 +367,10 @@ assume it's installed and `require' it."
 ;;
 ;; Emacs lisp
 ;;
+(unless --melpa
+  (progn
+    (require 'slime-autoloads)
+    (setq inferior-lisp-program "/usr/local/bin/sbcl")))
 (require-package 'elisp-slime-nav)
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'elisp-slime-nav-mode))
