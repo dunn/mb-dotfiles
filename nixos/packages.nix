10dc5f3e276fe7da6fae2864ee37adac05f02301
Alex Dunn <dunn.alex@gmail.com>
2016-11-08 06:55:15 -0800

    nixos: add sbcl and oracle jdk

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,6 +1,6 @@
 with (import <nixpkgs> {});
 {
-  inherit bashCompletion gnupg20 git notmuch openjdk silver-searcher tmux tig nodejs-6_x;
+  inherit bashCompletion gnupg20 git notmuch jdk sbcl silver-searcher tmux tig nodejs-6_x;
 
   emacs25 = pkgs.emacs25.override {
     withGTK2 = false;
