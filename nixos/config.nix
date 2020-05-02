4fe2308609cf335420ff1be7190c18a5507b24a1
Alex Dunn <dunn.alex@gmail.com>
2020-05-01 18:07:44 -0700

    make nix config work again

--- nixos/config.nix
+++ nixos/config.nix
@@ -1,1 +1,39 @@
-{ allowUnfree = true; }
+{
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+  };
+
+  allowUnfree = true;
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        bashCompletion
+        editorconfig-core-c
+        firefox
+        gcc
+        getmail
+        gnumake
+        gnupg
+        inconsolata
+        jdk
+        libxml2
+        libxslt
+        pass
+        pinentry
+        pkgconfig
+        postgresql
+        silver-searcher
+        slack
+        source-code-pro
+        sqlite
+        tig
+        tmux
+        xclip
+        youtube-dl
+        zoom-us
+      ];
+    };
+  };
+}
