5de623dbae1bd828aa7017e5b5f670f61628b1d6
Alex Dunn <dunn.alex@gmail.com>
2020-06-22 22:28:29 -0700

    magit: removed obsolete workaround

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -224,8 +224,6 @@ assume it's installed and `require' it."
 ;;
 ;; Git
 ;;
-;; https://github.com/magit/magit/issues/3997
-(require 'subr-x)
 (require-package 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
